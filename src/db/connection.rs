0346a615f2e8613849e08efa0a9227f9be80748e
rasulev.021 <rasulev.021@gmail.com>
2025-12-13 03:08:03 +0500

    fixed

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -2,7 +2,7 @@ use sqlx::{PgPool, postgres::PgPoolOptions};
 
 pub async fn create_pool(database_url: &str) -> PgPool {
     PgPoolOptions::new()
-        .max_connections(5)
+        .max_connections(100)
         .connect(database_url)
         .await
         .expect("Failed to create database pool")
