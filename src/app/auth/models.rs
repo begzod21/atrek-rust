d5d99771b1c74dcd2c0cf644d31f29acaadb718f
rasulev.021 <rasulev.021@gmail.com>
2025-12-12 20:50:38 +0500

    fx

--- src/app/auth/models.rs
+++ src/app/auth/models.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct AuthUser {
-    pub user_id: i64,
+    pub user_id: i32,
     pub username: String,
-    pub company_id: i64,
+    pub company_id: i32,
 }
