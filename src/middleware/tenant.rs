cc2994209151b1c1d88e39c317d83a9414ec8e57
rasulev.021 <rasulev.021@gmail.com>
2025-12-25 17:29:02 +0500

    check

--- src/middleware/tenant.rs
+++ src/middleware/tenant.rs
@@ -5,22 +5,21 @@ use axum::{
     http::{Request, StatusCode},
     middleware::Next,
     response::Response,
+    extract::State
 };
 use sqlx::PgPool;
 
-pub async fn tenant_middleware(mut req: Request<Body>, next: Next) -> Result<Response, StatusCode> {
+pub async fn tenant_middleware(
+    State(pool): State<PgPool>,
+    mut req: Request<Body>, 
+    next: Next
+) -> Result<Response, StatusCode> {
     let domain_url = req.headers()
         .get("host")
         .and_then(|v| v.to_str().ok())
         .unwrap_or("localhost")
         .to_string();
     println!("Domain URL: {}", domain_url);
-
-    let pool = req
-        .extensions()
-        .get::<PgPool>()
-        .cloned()
-        .ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
 
     let tenant = sqlx::query_as::<_, TenantCompany>(
             r#"SELECT id, schema_name, domain_url, cargo_distance 
